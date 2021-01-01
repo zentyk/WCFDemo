d3b3a0a27f3279daae90962107b882aff47f7bf6
Arturo Sanchez <asanchez@orthocana.com>
2020-12-31 20:15:21 -0600

    Added Client
    Signed-off-by: Arturo Sanchez <asanchez@orthocana.com>

---
+++ WPFSOAPClient/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace WPFSOAPClient {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            SoapReference.Service1Client oClient = new SoapReference.Service1Client();
+            string res = oClient.GetData(2);
+            MessageBox.Show(res);
+        }
+    }
+}
